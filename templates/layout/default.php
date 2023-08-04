cf7caf573b53342b34fa51eb71b97513fe38b1d8
Joe Boyle <49281513+jb-207@users.noreply.github.com>
2023-08-03 23:52:53 -0700

    title update

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,7 +14,7 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'Joe Boyle and CRIGenetics';
 ?>
 <!DOCTYPE html>
 <html>
